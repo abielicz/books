ae6f4f84a007f3c6b485d5557de49892c5224590
Ada <bielicza@gmail.com>
2017-06-27 21:48:57 +0200

    Try2

---
+++ app/web/index.php
@@ -0,0 +1,13 @@
+<?php
+
+umask(0000);
+//ini_set('display_errors', 0);
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', false);
+
+
+require_once __DIR__.'/../vendor/autoload.php';
+$app = require __DIR__.'/../src/app.php';
+require __DIR__.'/../config/prod.php';
+require __DIR__.'/../src/controllers.php';
+$app->run();
